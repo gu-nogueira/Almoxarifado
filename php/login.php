7f56347fd8c03845bed89fb6fd083dfef85e6288
Gustavo Nogueira <guuxtavim@gmail.com>
2021-03-22 23:18:49 -0300

    Backend: * lógica de sessão de usuário; OK
    Frontend:
    * tela inicial; OK
    * tela de perfil do usuário; OK
    * menu lateral; OK

--- php/login.php
+++ php/login.php
@@ -1,21 +1,16 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'proj_almox';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Falha ao conectar ao MySQL: ' . mysqli_connect_error());
-}
+include('connectdb.php');
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['Usuario'], $_POST['Senha']) ) {
 	// Could not get the data that should have been sent.
-	exit('Preencha os campos de login e senha!');
+	?>
+    <script type="text/javascript">
+    window.location.replace("../index.html");
+    alert("Preencha os campos de login e senha!");
+    </script>
+  <?php
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
@@ -29,6 +24,7 @@ if ($stmt = $con->prepare('SELECT idUsuarios, Senha FROM usuarios WHERE Usuario 
   if ($stmt->num_rows > 0) {
     $stmt->bind_result($idUsuarios, $Senha);
     $stmt->fetch();
+
     // Account exists, now we verify the password.
     // Note: remember to use password_hash in your registration file to store the hashed passwords.
     if (password_verify($_POST['Senha'], $Senha)) {
@@ -38,7 +34,7 @@ if ($stmt = $con->prepare('SELECT idUsuarios, Senha FROM usuarios WHERE Usuario 
       $_SESSION['loggedin'] = TRUE;
       $_SESSION['name'] = $_POST['Usuario'];
       $_SESSION['id'] = $idUsuarios;
-      echo 'Welcome ' . $_SESSION['name'] . '!';
+      header('Location: home.php');
     } else {
       // Incorrect password
       ?>
